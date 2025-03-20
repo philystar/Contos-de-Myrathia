3f266a6bb7912385ace3d5a4d7b9a09a2ea75186
Henrique Torres Rosa <zique.torres@gmail.com>
2025-03-20 00:21:34 -0300

    Concertando os inputs de imagem

--- scriptpic.js
+++ scriptpic.js
@@ -18,3 +18,27 @@ inputFile.addEventListener("change", function (e) {
     reader.readAsDataURL(file)
   } else (pictureImage.innerHTML = pictureImageTxt)
 })
+
+document.querySelectorAll(".pic_input").forEach(inputFile => {
+  const pictureImage = inputFile.closest(".img").querySelector(".pic_img");
+  const pictureImageTxt = "Escolha uma foto";
+  pictureImage.innerHTML = pictureImageTxt;
+
+  inputFile.addEventListener("change", function (e) {
+    const file = e.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.addEventListener("load", function (e) {
+        const img = document.createElement("img");
+        img.src = e.target.result;
+        img.classList.add("pic_img");
+        pictureImage.innerHTML = "";
+        pictureImage.appendChild(img);
+      });
+      reader.readAsDataURL(file);
+    } else {
+      pictureImage.innerHTML = pictureImageTxt;
+    }
+  });
+});
