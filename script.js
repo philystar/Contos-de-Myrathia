bbebf65d55da0e9986c8999cda3ad6358594479b
philystar <philipeahs3@gmail.com>
2025-03-19 23:48:26 -0300

    consertamos o negócio do erro do javascript que tava enchendo a PORRA do meu saco

--- script.js
+++ script.js
@@ -33,27 +33,6 @@ function calculateModifier(attribute) {
   document.getElementById(`${attribute}modifier`).value = modifier;
 }
 
-const inputFile = document.querySelector(".pic_input");
-const pictureImage = document.querySelector(".pic_img");
-const pictureImageTxt = "Escolha uma foto"
-pictureImage.innerHTML = pictureImageTxt;
-inputFile.addEventListener("change", function (e) {
-  const inputTarget = e.target
-  const file = inputTarget.files[0]
-  if (file) {
-    const reader = new FileReader()
-    reader.addEventListener("load", function (e) {
-      const readerTarget = e.target
-      const img = document.createElement("img")
-      img.src = readerTarget.result
-      img.classList.add("pic_img")
-      pictureImage.innerHTML = ""
-      pictureImage.appendChild(img)
-    })
-    reader.readAsDataURL(file)
-  } else (pictureImage.innerHTML = pictureImageTxt)
-})
-
 document.addEventListener("DOMContentLoaded", function () {
   const themeSelector = document.querySelector(".themeSelector");
   const root = document.documentElement;
